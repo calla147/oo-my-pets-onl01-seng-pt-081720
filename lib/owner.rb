9a7a3708c0e407c0c14b5bea27aa1c051d44525c
noella callahan <calla147@gmail.com>
2020-09-15 06:06:35 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,72 +1,86 @@
 require 'pry'
 
 class Owner
-  @@all= []
-  attr_accessor :name, :pets
-  attr_reader :species
-
-  def initialize(species)
-    @species = species
+  attr_reader :name, :species #cannot change owner's name and species
+  attr_accessor :cats # an owner has many cats: one-to-many relationship
+  attr_accessor :dogs # an owner has many dogs: one-to-many relationship
+  
+  @@all = []
+  
+  def initialize(name) 
+    @name = name
+    @species = "human"
     @@all << self
-    @pets = {:dogs => [], :cats => []}
+    self.cats = []
+    self.dogs = []
   end
-
+  
+  
   def say_species
-    return "I am a #{@species}."
+    "I am a #{@species}."
   end
-
-    def buy_dog(name_of_dog)
-    @pets[:dogs] << Dog.new(name_of_dog)
-  end
-
-  def buy_cat(name_of_cat)
-    @pets[:cats] << Cat.new(name_of_cat)
-  end
-
-  def walk_dogs
-    @pets.collect do |species, instances|
-      if species == :dogs
-        instances.each do |dog|
-          dog.mood = "happy"
-        end
-      end
-    end
-  end
-
-  def play_with_cats
-    @pets.collect do |species, instances|
-      if species == :cats
-        instances.each do |cat|
-          cat.mood = "happy"
-        end
-      end
-    end
-  end
-
-   def sell_pets
-    @pets.collect do |species, instances|
-      instances.each do |pet|
-        pet.mood = "nervous"
-      end
-      instances.clear
-    end
-  end
-
-  def list_pets
-    num_dogs = @pets[:dogs].size
-    num_cats = @pets[:cats].size
-    return "I have #{num_dogs} dog(s), and #{num_cats} cat(s)."
-  end
-
-  def self.all
+  
+  
+  def self.all 
     @@all
   end
-
-  def self.reset_all
-    @@all.clear
+  
+   
+   def self.count
+    self.all.length
   end
-
-  def self.count
-    @@all.size
+  
+  
+  def self.reset_all 
+    self.all.clear
+  end
+  
+  def buy_cat(cat_name)
+    
+    cat = Cat.all.find {|cat| cat.name == cat_name}
+    cat = Cat.new(cat_name, self) if cat.nil?
+    
+    cat.owner = self
+    self.cats << cat
+    self.cats.uniq!
+  end
+  
+  def buy_dog(dog_name)
+    
+    dog = Dog.all.find {|dog| dog.name == dog_name}
+    
+    if dog # exists
+      dog.owner = self
+      self.dogs << dog
+    else
+      dog = Dog.new(dog_name, self)  
+    end
+    # self.dogs.uniq!
+  end
+  
+    def walk_dogs
+    dogs.each do |dog|
+      dog.mood = "happy"
+    end
+  end
+  
+  def feed_cats
+    cats.map { |cat| cat.mood = "happy"}
+  end
+  
+  def sell_pets
+    
+    [self.cats, self.dogs].flatten.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+    
+    self.cats = []
+    self.dogs = []
+  end
+  
+  
+  def list_pets
+    return "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 end
