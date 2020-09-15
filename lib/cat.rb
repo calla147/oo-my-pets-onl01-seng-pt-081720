16f70378cc8d3ebf5e5d4b46615f3731598c9b89
noella callahan <calla147@gmail.com>
2020-09-15 06:03:42 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,20 +1,19 @@
 class Cat
   attr_reader :name
   attr_accessor :mood
-  attr_accessor :owner # cat belongs to one owner, many-to-one relationship
+  attr_accessor :owner  
   
   @@all = []
   
-  #Initialized with a name and an Owner object
-  def initialize(name, owner)
+    def initialize(name, owner)
     @name = name
     @owner = owner
-    @mood = "nervous" #initialize with nervous mood
+    @mood = "nervous" 
     @@all << self
     self.owner.cats << self
   end
   
-  def self.all #display all the cats
+  def self.all 
     @@all
   end
   
